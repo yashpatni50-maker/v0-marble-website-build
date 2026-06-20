ca81f6bde24d72cf66848193b7f1e60a35d55c04
Yash patni <yashpatni50@gmail.com>
2026-06-20 14:00:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,27 +39,8 @@ export default function RootLayout({
 }>) {
   return (
 <html lang="en" className={`${playfair.variable} ${inter.variable} bg-background`}>
-  <body className="font-sans antialiased">
-
-  <Script id="meta-pixel" strategy="afterInteractive">
-    {`
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}
-      (window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-
-      fbq('init', '1680601959684903');
-      fbq('track', 'PageView');
-    `}
-  </Script>
-
+ <body className="font-sans antialiased">
   {children}
-
   {process.env.NODE_ENV === 'production' && <Analytics />}
 </body>
 </html>
