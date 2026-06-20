a8ff3b6c1bbb01824f4006bb4123604b041743ca
Yash patni <yashpatni50@gmail.com>
2026-06-20 13:12:06 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,36 +41,27 @@ export default function RootLayout({
 <html lang="en" className={`${playfair.variable} ${inter.variable} bg-background`}>
   <body className="font-sans antialiased">
 
-    <Script id="meta-pixel" strategy="afterInteractive">
-      {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}
-        (window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
+  <Script id="meta-pixel" strategy="afterInteractive">
+    {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}
+      (window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
 
-        fbq('init', '1680601959684903');
-        fbq('track', 'PageView');
-      `}
-    </Script>
-    <noscript>
-  <img
-    height="1"
-    width="1"
-    style={{ display: 'none' }}
-    src="https://www.facebook.com/tr?id=1680601959684903&ev=PageView&noscript=1"
-    alt=""
-  />
-</noscript>
+      fbq('init', '1680601959684903');
+      fbq('track', 'PageView');
+    `}
+  </Script>
 
-    {children}
+  {children}
 
-    {process.env.NODE_ENV === 'production' && <Analytics />}
-  </body>
+  {process.env.NODE_ENV === 'production' && <Analytics />}
+</body>
 </html>
   )
 }
